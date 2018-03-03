d62bd5b852ff8556a24a5322f173ce7291e047e2
Matthew Bliffert <mattbliffert@icloud.com>
2018-03-03 12:38:36 -0500

    Added pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Matthew Bliffert | Full Stack Developer, Crafter of Digital Things',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
