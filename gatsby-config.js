61d1a23187321c9e751c58d9ae71b8de29568860
Matthew Bliffert <mattbliffert@icloud.com>
2018-03-07 20:45:09 -0500

    Removed unnecessary folders/files, setup Contentful integration, added eslint.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: 'Matthew Bliffert | Full Stack Developer, Crafter of Digital Things',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_TOKEN,
+      },
+    },
+  ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
